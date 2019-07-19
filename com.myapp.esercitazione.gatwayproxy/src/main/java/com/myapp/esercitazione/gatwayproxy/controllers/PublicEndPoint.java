0a0838351ebbbc2a530793effb81627c88aa03d2
Cerini Simone <cerini@eurisnet.it>
2019-07-19 17:24:09 +0200

    primo commit

---
+++ com.myapp.esercitazione.gatwayproxy/src/main/java/com/myapp/esercitazione/gatwayproxy/controllers/PublicEndPoint.java
@@ -0,0 +1,9 @@
+package com.myapp.esercitazione.gatwayproxy.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PublicEndPoint {
+//	@PostMapping("/")
+//	public 
+}
